369b81fde79a80f12b7b1ff5fde638c06ce04271
AYALABMDK <bmdk110@gmail.com>
2024-12-15 16:10:14 +0200

    start

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+
+// לייבא את express (מערכת מסגרת ליצירת שרתים)
+const express = require('express');
+const app = express();
+
+// הצגה פשוטה בעת ביקור בעמוד הראשי
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// הגדרת השרת לשמיעה על פורט 3000 (או פורט אחר שתרצה)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
